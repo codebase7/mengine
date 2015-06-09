774b37854e65fa79fb417aecded7b1277060e245
codebase7 <codebase7@yahoo.com>
2015-06-09 11:30:11 -0400

    Update FileUtills_Emulated_Symobilic_Link_Functions header.
    This commit updates the FileUtills_Emulated_Symobilic_Link_Functions header
    (FileUtills_Emulated_Symobilic_Link_Functions.h)
    to be consistant with the new API changes.

--- src/Common/Src/File_Management_Subsystem/FileUtills_Emulated_Symbolic_Link_Functions.h
+++ src/Common/Src/File_Management_Subsystem/FileUtills_Emulated_Symbolic_Link_Functions.h
@@ -19,49 +19,52 @@
     https://github.com/codebase7/mengine    
 */
 
-// Include guard.
+/* Include guard. */
 #ifndef FILEUTILLS_EMULATED_SYMBOLIC_LINK_FUNCTIONS_H
 #define FILEUTILLS_EMULATED_SYMBOLIC_LINK_FUNCTIONS_H
 
-// Internal includes.
+/* Internal includes. */
 #include "../Error_Handler/Common_Error_Handler.h"
 #include "../Error_Handler/Common_Error_Handler_Internal.h"
 
-// External includes.
+/* External includes. */
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdlib.h>
 
+/* Define magic string for the emulated symbolic link file. */
+#define MSYS_EMU_SYMLINK_MAGIC "MSYS_EMU_SYMLINK"
+
+/* Define file extension for the emulated symbolic link file. */
+#define MSYS_EMU_SYMLINK_FILE_EXT "msl"
+
+/* Define size of the byte count field. */
+#define MSYS_EMU_SYMLINK_BYTE_COUNT_FIELD_SIZE 4
 
 /*!
  * 	Description of the emulated symbolic link file format:
  *
  * 	Offset 0: Magic string. (E.g. "MSYS_EMU_SYMLINK")
  *
- * 	Offset (0 + sizeof(<Magic string>)): uint (4 bytes, little endian) size of the length value. (I.e. sizeof(size_t)).
+ * 	Offset (0 + sizeof(<Magic string>)): unsigned 32-bit integer (4 bytes, big endian) size of the length value. (I.e. sizeof(size_t)).
  * 
  * 	Offset (0 + sizeof(<Magic string>) + 4): length of the link. (Length is the same as the read size variable.)
  *
  * 	Offset (0 + sizeof(<Magic string>) + 4 + (size of the length value)): Start of link.
  *
- * 	Offset (0 + sizeof(<Magic string>) + 4 + (size of the length value) + <length of the link>): End of the link, and enf of file.
+ * 	Offset (0 + sizeof(<Magic string>) + 4 + (size of the length value) + <length of the link>): End of the link, and end of file.
  */
 
 
 // Define namespace.
 namespace FileUtills
-{
-	// Define magic string for the emulated symbolic link file.
-	#define MSYS_EMU_SYMLINK_MAGIC "MSYS_EMU_SYMLINK"
-
-	// Define file extension for the emulated symbolic link file.
-	#define MSYS_EMU_SYMLINK_FILE_EXT "msl"
-
+{	
 	// Define functions.
 
 	/*!
-	 * 	int FileUtills::Create_MSYS_Emulated_Symbolic_Link(const std::string & linkDestionation, const std::string & pathToLinkFile)
+	 * 	int FileUtills::Create_MSYS_Emulated_Symbolic_Link(const char * linkDestionation, const size_t linkDestionationSize,
+	 *													   const char * pathToLinkFile, const size_t pathToLinkFileSize)
 	 * 
 	 * 	Takes the given linkDestionation string and creates an emulated symbolic link file with the string's
 	 * 	contents at the given path on the filesystem.
@@ -72,21 +75,19 @@ namespace FileUtills
 	 * 	need to create the directory tree leading up to the file to create.) If either of these assumptions
 	 * 	are false, this function will abort and return an error.
 	 *
-	 * 	If successfull this function will return COMMON_ERROR_SUCCESS, and the emulated symbolic link file
+	 * 	If successful this function will return COMMON_ERROR_SUCCESS, and the emulated symbolic link file
 	 * 	will be present at the given location on the filesystem.
 	 *
-	 * 	If an error occurs, the approperite Common namespace error code will be returned. In addition, the
+	 * 	If an error occurs, the appropriate Common name-space error code will be returned. In addition, the
 	 * 	emulated link file will be rendered invalid on the filesystem. (It will still be present on the
 	 * 	filesystem however, as such it will need to be deleted manually.)
-	 *
-	 * 	In all instances the returned error code will be copied to Common::commonLastErrorCode. (In addition,
-	 * 	depending on the configured log level, a human readable error message will be outputted to the logging
-	 * 	function to better describe the error that occured.)
 	 */
-	int Create_MSYS_Emulated_Symbolic_Link(const std::string & linkDestionation, const std::string & pathToLinkFile);
+	int Create_MSYS_Emulated_Symbolic_Link(const char * linkDestionation, const size_t linkDestionationSize,
+										   const char * pathToLinkFile, const size_t pathToLinkFileSize);
 
 	/*!
-	 * 	int FileUtills::Read_MSYS_Emulated_Symbolic_Link(std::string & link, const std::string & pathToLinkFile)
+	 * 	int FileUtills::Read_MSYS_Emulated_Symbolic_Link(char ** retStr, size_t * retStrSize, const char * pathToLinkFile, 
+	 *													 const size_t pathToLinkFileSize)
 	 *
 	 * 	Erases the given link argument string, then reads the file from the given path for a symbolic link
 	 * 	string, and writes it to the given link argument string.
@@ -94,9 +95,9 @@ namespace FileUtills
 	 * 	NOTE: This function does NOT resolve the read symbolic link, and will return it as is on the filesystem.
 	 * 	To resolve the link, either call your own resolver function, or call FileUtills::ResolvePath().
 	 *
-	 * 	NOTE: This function is called automaticly by FileUtills::ResolvePath() if symbolic link resolution is
+	 * 	NOTE: This function is called automatically by FileUtills::ResolvePath() if symbolic link resolution is
 	 * 	enabled by the caller (it is by default), and an emulated link file is found during path resolution.
-	 * 	As such it's not nessacarry to call this function directly before FileUtills::ResolvePath().
+	 * 	As such it's not necessary to call this function directly before FileUtills::ResolvePath().
 	 *
 	 * 	(Sidenote: This function returns read links as is, if the read link is relative to the directory it is in,
 	 * 	 then the link string will need to have the path to the directory that the emulated link file is stored
@@ -105,20 +106,17 @@ namespace FileUtills
 	 * 	 calling program's current working directory. (Along with all of the issues that come with that
 	 * 	 assumption.))
 	 *
-	 * 	If successfull, the given link argument string will have it's contents replaced with the read link
+	 * 	If successful, the given link argument string will have it's contents replaced with the read link
 	 * 	string, and COMMON_ERROR_SUCCESS will be returned.
 	 *
-	 * 	If an error occurs then the given link argument string will be blank / empty, and the approperite
-	 * 	Common namespace error code will be returned.
-	 *
-	 * 	In all instances the returned error code will be copied to Common::commonLastErrorCode. (In addition,
-	 * 	depending on the configured log level, a human readable error message will be outputted to the logging
-	 * 	function to better describe the error that occured.)
+	 * 	If an error occurs then the given link argument string will be blank / empty, and the appropriate
+	 * 	Common name-space error code will be returned.
 	 */
-	int Read_MSYS_Emulated_Symbolic_Link(std::string & link, const std::string & pathToLinkFile);
+	int Read_MSYS_Emulated_Symbolic_Link(char ** retStr, size_t * retStrSize, const char * pathToLinkFile, const size_t pathToLinkFileSize);
 
 	/*!
-	 * 	int FileUtills::Update_MSYS_Emulated_Symbolic_Link(const std::string & linkDestionation, const std::string & pathToLinkFile)
+	 * 	int FileUtills::Update_MSYS_Emulated_Symbolic_Link(const char * linkDestionation, const size_t linkDestionationSize,
+	 *													   const char * pathToLinkFile, const size_t pathToLinkFileSize)
 	 *
 	 * 	This function updates a pre-existing emulated link file at the given path on the filesystem, with the contents
 	 * 	of the given linkDestionation argument string.
@@ -127,20 +125,17 @@ namespace FileUtills
 	 * 	expects that the emulated link file will be valid. If either of these two conditions are not true, then this function
 	 * 	will abort and return an error.
 	 *
-	 * 	If successfull, the given emulated link file will contain the contents of the given linkDestionation argument string, and
+	 * 	If successful, the given emulated link file will contain the contents of the given linkDestionation argument string, and
 	 * 	the returned error code will be COMMON_ERROR_SUCCESS.
 	 *
-	 * 	If an error occurs, the approperite Common namespace error code will be returned. In addition, the
+	 * 	If an error occurs, the appropriate Common name-space error code will be returned. In addition, the
 	 * 	emulated link file will be rendered invalid on the filesystem. (It will still be present on the
 	 * 	filesystem however, as such it will need to be deleted manually.)
-	 *
-	 * 	In all instances the returned error code will be copied to Common::commonLastErrorCode. (In addition,
-	 * 	depending on the configured log level, a human readable error message will be outputted to the logging
-	 * 	function to better describe the error that occured.)
 	 */
-	int Update_MSYS_Emulated_Symbolic_Link(const std::string & linkDestionation, const std::string & pathToLinkFile);
+	int Update_MSYS_Emulated_Symbolic_Link(const char * linkDestionation, const size_t linkDestionationSize,
+										   const char * pathToLinkFile, const size_t pathToLinkFileSize);
 };
 
-#endif
+#endif	/* FILEUTILLS_EMULATED_SYMBOLIC_LINK_FUNCTIONS_H */
 
-// End of FileUtills_Emulated_Symbolic_Link_Functions.h
+/* End of FileUtills_Emulated_Symbolic_Link_Functions.h */
