1e68239ebfcd060ca6496004d2d311bdc68b350d
codebase7 <codebase7@yahoo.com>
2015-08-07 02:52:54 -0400

    Add FileUtills_Private_API_Windows_Syscall.h header.
    This commit adds the FileUtills_Private_API_Windows_Syscall.h header to FileUtills.

---
+++ src/Common/Src/File_Management_Subsystem/FileUtills_Private_API_Windows_Syscall.h
@@ -0,0 +1,32 @@
+/*!
+	Multiverse Engine Project 06/7/2015 FileUtills FileUtills_Private_API_Windows_Syscall.h
+
+	Copyright (C) 2015 Multiverse Engine Project
+
+	This program is free software;
+	you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+	either version 2 of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with this program;
+	if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+	Official source repository and project information can be found at
+	https://github.com/codebase7/mengine
+*/
+
+/* Include guard. */
+#ifndef FILEUTILLS_PRIVATE_API_WINDOWS_SYSCALL_H
+#define FILEUTILLS_PRIVATE_API_WINDOWS_SYSCALL_H
+
+/* Define CSIDL values. */
+#define FILEUTILLS_USER_PROFILE_ID CSIDL_PROFILE
+#define FILEUTILLS_USER_DOCUMENTS_ID CSIDL_PERSONAL
+#define FILEUTILLS_APPDATA_ID CSIDL_LOCAL_APPDATA
+
+#endif	/* FILEUTILLS_PRIVATE_API_WINDOWS_SYSCALL_H */
+
+/* End of FileUtills_Private_API_Windows_Syscall.h. */
