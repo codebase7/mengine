ba2d9c7504dc1b063659235bb521cf561f1f507d
codebase7 <codebase7@yahoo.com>
2016-01-17 04:34:58 -0500

    Fix return codes for Common_Dynamic_Library_Subsystem_Load_Library() (Posix)
    This commit fixes the return codes for Common_Dynamic_Library_Subsystem_Load_Library() (Posix) to use the Common Namespace Error Codes. (In addition to translating returned host error codes.)
    Also this commit fixes the code comment style in Common_Dynamic_Library_Subsystem_Load_Library() (Posix), in addition to adding verbose logging support.

--- src/Common/Src/Dynamic_Library_Subsystem/Dynamic_Library_Subsystem_POSIX.c
+++ src/Common/Src/Dynamic_Library_Subsystem/Dynamic_Library_Subsystem_POSIX.c
@@ -18,92 +18,121 @@
     https://github.com/codebase7/mengine
 */
 
+/* Check for linux. */
 #ifdef __linux__
+
+/* Internal includes */
 #include "Dynamic_Library_Subsystem.h"
+#include "../Error_Handler/Common_Error_Handler_Structures.h"
+#include "../Error_Handler/Posix_Error_Translation_Table.h"
+
+/* External includes. */
 #include <dlfcn.h>	// dlopen, dlclose, dlsym, dlerror.
 
+/* Check for C++ Compiler. */
 #ifdef __cplusplus
-// Define extern C.
+/* Define extern C. */
 extern "C" {
 #endif
 		int Common_Dynamic_Library_Subsystem_Load_Library(const char * pathToLibrary, const bool reloadLibrary, Common_Dynamic_Library_Subsystem_Loaded_Dynamic_Library *const lib)
 		{
-				// Init vars.
-				int result = 0;				// The result of this function.
-				void * callResult = NULL;		// The result of the call to dlopen().
+				/* Init vars. */
+				int ret = COMMON_ERROR_UNKNOWN_ERROR;			/* The result of this function. */
+				int retFromCall = COMMON_ERROR_UNKNOWN_ERROR;	/* The result of calls to other engine functions. */
+				int errcpy = 0;									/* The current errno. */
+				void * callResult = NULL;						/* The result of the call to dlopen(). */
 
-				// Check to see if the pointer to the management structure is valid.
+				/* Check to see if the pointer to the management structure is valid. */
 				if (lib != NULL)
 				{
-						// Check to see if pathToLibrary is NULL.
+						/* Check to see if pathToLibrary is NULL. */
 						if (pathToLibrary != NULL)
 						{
-								// Check to see if the library is loaded.
+								/* Check to see if the library is loaded. */
 								if ((!lib->bIsLoaded) || (reloadLibrary))
 								{
-										// Check and see if the library is loaded.
+										/* Check and see if the library is loaded. */
 										if (lib->bIsLoaded)
 										{
-												// Call Unload_Library.
-												result = Common_Dynamic_Library_Subsystem_Unload_Library(lib);
+												/* Call Unload_Library. */
+												retFromCall = Common_Dynamic_Library_Subsystem_Unload_Library(lib);
 										}
 
-										// Only continue if the library was unloaded, or if we did not need to unload the library.
-										if (result == 0)
+										/* Only continue if the library was unloaded, or if we did not need to unload the library. */
+										if ((retFromCall == COMMON_ERROR_UNKNOWN_ERROR) || (retFromCall == COMMON_ERROR_SUCCESS))
 										{
-												// Set the values in lib.
+												/* Set the values in lib. */
 												lib->bIsLoaded = false;
 												lib->bLastCallEncounteredAnError = false;
 												lib->osSpecificPointerData = NULL;
 												lib->pathToLibrary = pathToLibrary;
 
-												// Call dlopen().
+												/* Call dlopen(). */
 												callResult = dlopen(lib->pathToLibrary, RTLD_LAZY | RTLD_LOCAL);
 
-												// Check the callResult.
+												/* Check the callResult. */
 												if (callResult == NULL)
 												{
-														// Could not load the library.
-														result = -1;
+														/* Fetch the errno. */
+														errcpy = errno;
+
+														/* Translate the error code. */
+														retFromCall = Common_Translate_Posix_Errno_To_Common_Error_Code(errcpy);
+
+														/* Could not load the library. */
+														ret = retFromCall;
 														lib->bLastCallEncounteredAnError = true;
+														COMMON_LOG_VERBOSE("Common_Dynamic_Library_Subsystem_Load_Library(): Could not load <");
+														COMMON_LOG_VERBOSE(lib->pathToLibrary);
+														COMMON_LOG_VERBOSE("> Host function returned: ");
+														COMMON_LOG_VERBOSE(Common_Get_Error_Message(retFromCall));
 												}
 												else
 												{
-														// Cast the OS specific data structure pointer to void*.
+														/* Cast the OS specific data structure pointer to void. */
 														lib->osSpecificPointerData = callResult;
 
-														// Set bIsLoaded.
+														/* Set bIsLoaded. */
 														lib->bIsLoaded = true;
+
+														/* Success. */
+														ret = COMMON_ERROR_SUCCESS;
+														COMMON_LOG_VERBOSE("Common_Dynamic_Library_Subsystem_Load_Library(): <");
+														COMMON_LOG_VERBOSE(lib->pathToLibrary);
+														COMMON_LOG_VERBOSE("> loaded.");
 												}
 										}
 										else
 										{
-												// Encountered an error during the unload.
-												result = -2;
+												/* Encountered an error during the unload. */
+												ret = retFromCall;
 												lib->bLastCallEncounteredAnError = true;
+												COMMON_LOG_VERBOSE("Common_Dynamic_Library_Subsystem_Load_Library(): Unable to reload library.");
 										}
 								}
 								else
 								{
-										// Library is already loaded.
-										result = 1;
+										/* Library is already loaded. */
+										ret = DYNLIB_ERROR_LIBRARY_ALREADY_LOADED;
 								}
 						}
 						else
 						{
-								// pathToLibrary is NULL.
-								result = -3;
+								/* pathToLibrary is NULL. */
+								ret = COMMON_ERROR_INVALID_ARGUMENT;
 								lib->bLastCallEncounteredAnError = true;
+								COMMON_LOG_VERBOSE("Common_Dynamic_Library_Subsystem_Load_Library(): No path to the library was given. Unable to load a library without the path to it.");
 						}
 				}
 				else
 				{
-						// Management structure is invalid.
-						result = -4;
+						/* Management structure is invalid. */
+						ret = COMMON_ERROR_INVALID_ARGUMENT;
+						COMMON_LOG_VERBOSE("Common_Dynamic_Library_Subsystem_Load_Library(): The engine's library structure for the given library is invalid. Unable to load a library without a valid library structure.");
 				}
 
-				// Return result.
-				return result;
+				/* Return result. */
+				return ret;
 		}
 
 		int Common_Dynamic_Library_Subsystem_Unload_Library(Common_Dynamic_Library_Subsystem_Loaded_Dynamic_Library *const lib)
