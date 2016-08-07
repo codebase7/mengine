fc9356323b083a067fd821518d9bb0de2b1dfd3e
codebase7 <codebase7@yahoo.com>
2016-08-07 18:39:12 -0400

    Convert Renderer_Basic_Functions.h to C linkage.
    This commit converts Renderer_Basic_Functions.h to support C linkage. (Also fixes the copyright date, and some comments.)

--- src/Common/Src/Rendering_Subsystem/Renderer_Basic_Functions.h
+++ src/Common/Src/Rendering_Subsystem/Renderer_Basic_Functions.h
@@ -1,7 +1,7 @@
 /*
-	Multiverse Engine Project	 Common Rendering_Subsystem Renderer_Basic_Functions.h	20/12/2013
+	Multiverse Engine Project Common Rendering_Subsystem Renderer_Basic_Functions.h	20/12/2013
 
-	Copyright (C) 2014 Multiverse Engine Project
+	Copyright (C) 2016 Multiverse Engine Project
 
 	This program is free software;
 	you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
@@ -18,16 +18,70 @@
 	https://github.com/codebase7/mengine
 */
 
-// Include guard.
+/* Include guard. */
 #ifndef COMMON_RENDER_SUBSYSTEM_H
 #error "You must include the Renderer.h header file. It will include all of the other needed headers."
-#else
+#else	/* COMMON_RENDER_SUBSYSTEM_H */
 
 #ifndef COMMON_RENDER_BASIC_FUNCTIONS_H
 #define COMMON_RENDER_BASIC_FUNCTIONS_H
 
 /* Include DLL_PORT.h */
 #include "../../../DLL_PORT.h"
+
+/* Define C functions. */
+/*!
+ *	int Common_Renderer_Get_Rendering_Subsystem_API_Major_Version_Number()
+ *
+ *	Returns the major version number of the Rendering Subsystem.
+ */
+MSYS_DLL_EXPORT int Common_Renderer_Get_Rendering_Subsystem_API_Major_Version_Number();
+
+/*!
+ *	int Common_Renderer_Get_Rendering_Subsystem_API_Minor_Version_Number()
+ *
+ *	Returns the minor version number of the Rendering Subsystem.
+ */
+MSYS_DLL_EXPORT int Common_Renderer_Get_Rendering_Subsystem_API_Minor_Version_Number();
+
+/*!
+ *	int Common_Renderer_Get_Rendering_Subsystem_API_Revision_Version_Number()
+ *
+ *	Returns the revision version number of the Rendering Subsystem.
+ */
+MSYS_DLL_EXPORT int Common_Renderer_Get_Rendering_Subsystem_API_Revision_Version_Number();
+
+/*!
+ * 	bool Common_Renderer_Calculate_Offsets_From_Coordnates(const long destXCoordOffset, const long destYCoordOffset, const size_t destXAxisResolution, const size_t destYAxisResolution,
+ * 								 const size_t sourceXAxisResolution, const size_t sourceYAxisResolution, size_t * currentSourceXCoordOffset, size_t * currentDestXCoordOffset, size_t * currentDestOffset,
+ * 								 size_t * currentSourceOffset, size_t * nextDestEndOfLineOffset, size_t * nextSourceEndOfLineOffset)
+ *
+ * 	Calculates offsets for use by the overlay system and renderer.
+ *
+ * 	If a given coordnate is negative it will be made positive prior to calculating the offset. (This will not change the given coordnate outside of the function.)
+ *
+ * 	Pram: const long & destXCoordOffset, the offset from the destionation's x axis origin point where the source's x axis origin point will be placed.
+ * 	Pram: const long & destYCoordOffset, the offset from the destionation's y axis origin point where the source's y axis origin point will be placed.
+ * 	Pram: const size_t & destXAxisResolution, the resolution of the destionation's x (horazontal) axis.
+ * 	Pram: const size_t & destYAxisResolution, the resolution of the destionation's y (vertical) axis.
+ * 	Pram: const size_t & sourceXAxisResolution, the resolution of the source's x (horazontal) axis.
+ * 	Pram: const size_t & sourceYAxisResolution, the resolution of the source's y (vertical) axis.
+ * 	Pram: size_t & currentSourceXCoordOffset, The amount the Overlay::Blit() function must jump forward when moving to a new line in the source.
+ * 	Pram: size_t & currentDestXCoordOffset, The amount the Overlay::Blit() function must jump forward when moving to a new line in the destionation.
+ * 	Pram: size_t & currentDestOffset, the offset in the destionation where we will start blitting data to.
+ * 	Pram: size_t & currentSourceOffset, the offset in the source where we will start blitting data from.
+ * 	Pram: size_t & nextDestEndOfLineOffset, the offset on the destionation where the first line we blit to will end.
+ * 	Pram: size_t & nextSourceEndOfLineOffset, the offset on the source where the first line we blit from will end.
+ *
+ * 	Returns true if the source will show up on destionation. (The needed offsets will be in their respective arguments.)
+ * 	Returns false otherwise. (All of the non-const offset arguments will be set to zero as well.)
+ */
+MSYS_DLL_EXPORT bool Common_Renderer_Calculate_Offsets_From_Coordnates(const long destXCoordOffset, const long destYCoordOffset, const size_t destXAxisResolution, const size_t destYAxisResolution,
+																		const size_t sourceXAxisResolution, const size_t sourceYAxisResolution, size_t * currentSourceXCoordOffset, size_t * currentDestXCoordOffset, size_t * currentDestOffset,
+																		size_t * currentSourceOffset, size_t * nextDestEndOfLineOffset, size_t * nextSourceEndOfLineOffset);
+
+/* Check for C++ compiler. */
+#ifdef __cplusplus
 
 // Declare namespaces.
 namespace Common
@@ -38,6 +92,8 @@ namespace Common
 		 *	int Common::Renderer::Get_Rendering_Subsystem_API_Major_Version_Number()
 		 *
 		 *	Returns the major version number of the Rendering Subsystem.
+		 *
+		 *	(Wrapper to Common_Renderer_Get_Rendering_Subsystem_API_Major_Version_Number().)
 		 */
 		MSYS_DLL_EXPORT int Get_Rendering_Subsystem_API_Major_Version_Number();
 
@@ -45,6 +101,8 @@ namespace Common
 		 *	int Common::Renderer::Get_Rendering_Subsystem_API_Minor_Version_Number()
 		 *
 		 *	Returns the minor version number of the Rendering Subsystem.
+		 *
+		 *	(Wrapper to Common_Renderer_Get_Rendering_Subsystem_API_Minor_Version_Number().)
 		 */
 		MSYS_DLL_EXPORT int Get_Rendering_Subsystem_API_Minor_Version_Number();
 
@@ -52,6 +110,8 @@ namespace Common
 		 *	int Common::Renderer::Get_Rendering_Subsystem_API_Revision_Version_Number()
 		 *
 		 *	Returns the revision version number of the Rendering Subsystem.
+		 *
+		 *	(Wrapper to Common_Renderer_Get_Rendering_Subsystem_API_Revision_Version_Number().)
 		 */
 		MSYS_DLL_EXPORT int Get_Rendering_Subsystem_API_Revision_Version_Number();
 
@@ -60,33 +120,16 @@ namespace Common
 		 * 								 const size_t & sourceXAxisResolution, const size_t & sourceYAxisResolution, size_t & currentSourceXCoordOffset, size_t & currentDestXCoordOffset, size_t & currentDestOffset,
 		 * 								 size_t & currentSourceOffset, size_t & nextDestEndOfLineOffset, size_t & nextSourceEndOfLineOffset)
 		 *
-		 * 	Calculates offsets for use by the overlay system and renderer.
-		 *
-		 * 	If a given coordnate is negative it will be made positive prior to calculating the offset. (This will not change the given coordnate outside of the function.)
-		 *
-		 * 	Pram: const long & destXCoordOffset, the offset from the destionation's x axis origin point where the source's x axis origin point will be placed.
-		 * 	Pram: const long & destYCoordOffset, the offset from the destionation's y axis origin point where the source's y axis origin point will be placed.
-		 * 	Pram: const size_t & destXAxisResolution, the resolution of the destionation's x (horazontal) axis.
-		 * 	Pram: const size_t & destYAxisResolution, the resolution of the destionation's y (vertical) axis.
-		 * 	Pram: const size_t & sourceXAxisResolution, the resolution of the source's x (horazontal) axis.
-		 * 	Pram: const size_t & sourceYAxisResolution, the resolution of the source's y (vertical) axis.
-		 * 	Pram: size_t & currentSourceXCoordOffset, The amount the Overlay::Blit() function must jump forward when moving to a new line in the source.
-		 * 	Pram: size_t & currentDestXCoordOffset, The amount the Overlay::Blit() function must jump forward when moving to a new line in the destionation.
-		 * 	Pram: size_t & currentDestOffset, the offset in the destionation where we will start blitting data to.
-		 * 	Pram: size_t & currentSourceOffset, the offset in the source where we will start blitting data from.
-		 * 	Pram: size_t & nextDestEndOfLineOffset, the offset on the destionation where the first line we blit to will end.
-		 * 	Pram: size_t & nextSourceEndOfLineOffset, the offset on the source where the first line we blit from will end.
-		 *
-		 * 	Returns true if the source will show up on destionation. (The needed offsets will be in their respective arguments.)
-		 * 	Returns false otherwise. (All of the non-const offset arguments will be set to zero as well.)
+		 * 	(Wrapper to Common_Renderer_Calculate_Offsets_From_Coordnates().)
 		 */
 		bool Calculate_Offsets_From_Coordnates(const long & destXCoordOffset, const long & destYCoordOffset, const size_t & destXAxisResolution, const size_t & destYAxisResolution,
 						       const size_t & sourceXAxisResolution, const size_t & sourceYAxisResolution, size_t & currentSourceXCoordOffset, size_t & currentDestXCoordOffset, size_t & currentDestOffset,
 						       size_t & currentSourceOffset, size_t & nextDestEndOfLineOffset, size_t & nextSourceEndOfLineOffset);
 	};
 };
+#endif	/* __cplusplus */
 
-#endif
-#endif
+#endif	/* COMMON_RENDER_BASIC_FUNCTIONS_H */
+#endif	/* COMMON_RENDER_SUBSYSTEM_H */
 
-// End of Renderer_Basic_Functions.h
+/* End of Renderer_Basic_Functions.h */
