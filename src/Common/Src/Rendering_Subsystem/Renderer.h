01fbfdd9064ee459c57ed75e1610bcbb8c8e99d5
codebase7 <codebase7@yahoo.com>
2016-08-07 18:39:12 -0400

    Convert Renderer.h to support C linking.
    This commit converts Renderer.h to support C linkage. (Also fixes the copyright date, and some comment typos.)

--- src/Common/Src/Rendering_Subsystem/Renderer.h
+++ src/Common/Src/Rendering_Subsystem/Renderer.h
@@ -1,10 +1,10 @@
 /*
-	Multiverse Engine Project	 Common Rendering_Subsystem Renderer.h	20/12/2013
+	Multiverse Engine Project Common Rendering_Subsystem Renderer.h	20/12/2013
 
 	This is the base header for the rendering subsystem.
 	You should only include this header when using the rendering subsystem.
 
-	Copyright (C) 2014 Multiverse Engine Project
+	Copyright (C) 2016 Multiverse Engine Project
 
 	This program is free software;
 	you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
@@ -21,33 +21,44 @@
 	https://github.com/codebase7/mengine
 */
 
-// Include guard.
+/* Include guard. */
 #ifndef COMMON_RENDER_SUBSYSTEM_H
 #define COMMON_RENDER_SUBSYSTEM_H
 
-// Standard library includes.
+/* Check for C++ compiler. */
+#ifdef __cplusplus
+
+/* C++ Standard library includes. */
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
 #include <vector>
 
-// Internal library header includes.
-#include "../Error_Handler/Common_Error_Handler.h"	// Used for error handling.
-#include "../../../Core/Src/DataProcess.h"		// Used for DataProcess::removeFromVector().
+/* Check for C compiler. */
+#else	/* __cplusplus */
 
-// Internal includes.
-#include "Renderer_Basic_Functions.h"				// Functions that are used in both the renderer and overlay classes.
-#include "Renderer_Data_Structures.h"				// Renderer base class.
-#include "Renderer_Data_Structures_Overlay_Base_Functions.h"	// Base functions for the overlay template class defined in Renderer_Data_Structures.h.
-#include "Renderer_Management_Functions.h"			// Static renderer functions. (Create_Renderer(), Choose_Renderer(), etc.)
-#include "Renderer_Text_Console.h"				// Text Console Renderer.
+/* C includes. */
+#include <stddef.h>
+#include <stdio.h>
+
+#endif	/* __cplusplus */
+
+/* Internal library header includes. */
+#include "../Error_Handler/Common_Error_Handler.h"	/* Used for error handling. */
+#include "../../../Core/Src/DataProcess.h"		/* Used for DataProcess::removeFromVector(). */
+
+/* Internal includes. */
+#include "Renderer_Basic_Functions.h"				/* Functions that are used in both the renderer and overlay classes. */
+#include "Renderer_Data_Structures.h"				/* Renderer base class. */
+#include "Renderer_Data_Structures_Overlay_Base_Functions.h"	/* Base functions for the overlay template class defined in Renderer_Data_Structures.h. */
+#include "Renderer_Management_Functions.h"			/* Static renderer functions. (Create_Renderer(), Choose_Renderer(), etc.) */
+#include "Renderer_Text_Console.h"				/* Text Console Renderer. */
 
 /* Define API Version. */
 #define MSYS_RENDERING_SUBSYS_API_MAJOR_VER 0
 #define MSYS_RENDERING_SUBSYS_API_MINOR_VER 0
 #define MSYS_RENDERING_SUBSYS_API_REVISION_VER 1
 
-#endif
+#endif	/* COMMON_RENDER_SUBSYSTEM_H */
 
-// End of Renderer.h
-
+/* End of Renderer.h */
